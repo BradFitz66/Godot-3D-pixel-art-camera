c63ea5a7a876dd1c3a8e62ee42cfc9692c6645fd
denovodavid <d@vidhol.land>
2023-09-22 09:21:20 +1000

    ✨ some updates :)

--- Code/Miscellaneous/SimpleCharacterController.gd
+++ Code/Miscellaneous/SimpleCharacterController.gd
@@ -5,32 +5,35 @@ extends CharacterBody3D
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta):
-    #Get the forward and right vectors of the camera
-    var forward = camera.global_transform.basis.z
-    forward.y = 0
-    var right = camera.global_transform.basis.x
-    right.y = 0
-   
-    #Get the input
-    var input:Vector2 = Input.get_vector("left","right","forwards","backwards")
-    var direction:Vector3 = Vector3(input.x,0,input.y)
+	#Get the forward and right vectors of the camera
+	var forward = camera.global_transform.basis.z
+	forward.y = 0
+	var right = camera.global_transform.basis.x
+	right.y = 0
 
-    #Make direction relative to the camera
-    direction = forward * direction.z + right * direction.x
-    direction = direction.normalized()
+	#Get the input
+	var input:Vector2 = Input.get_vector("left","right","forwards","backwards")
+	var direction:Vector3 = Vector3(input.x,0,input.y)
 
-    if Input.is_action_just_pressed("jump") and is_on_floor():
-        velocity.y += 5
+	#Make direction relative to the camera
+	direction = forward * direction.z + right * direction.x
+	direction = direction.normalized()
 
-    #Set velocity and move
-    velocity = Vector3(
-                    direction.x * speed,
-                    velocity.y - gravity * delta,
-                    direction.z * speed
-                )
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y += 5
 
-    #Jump when space is pressed
+	if(Input.is_action_just_pressed("right_mouse")):
+		rotation_degrees.y += 45;
+
+	#Set velocity and move
+	velocity = Vector3(
+					direction.x * speed,
+					velocity.y - gravity * delta,
+					direction.z * speed
+				)
+
+	#Jump when space is pressed
 
 
-    move_and_slide()     
+	move_and_slide()
 
--- Code/Rendering/3DPixelCam.gd
+++ Code/Rendering/3DPixelCam.gd
@@ -1,34 +1,21 @@
+class_name Camera3DTexelSnapped
 extends Camera3D
 
-#Resolution is 320x180 with some padding to account for the screen texel error
-const width: int = 428
-const height: int = 242
+var texel_error: Vector2
 
-const orthographic_size: float = 8
-const texel_snap: float = float(height) / orthographic_size
+var _last_rot: Vector3
+var _snap_space: Transform3D
 
-@export var cam_proxy: Node3D
-@export var cam: Node3D
-@export  var render_texture : TextureRect 
 
-var last_rot :Vector3
-var snap_space : Transform3D
-#Create a transform with a 45 degree rotation on the y axis
-func _ready():
-    last_rot = cam_proxy.global_rotation
-    snap_space = cam_proxy.global_transform
-
-func _process(_delta): 
-    if cam_proxy.global_rotation != last_rot: 
-        last_rot = cam_proxy.global_rotation 
-        snap_space = cam_proxy.global_transform
-
-    var snap_space_pos := cam_proxy.global_position * snap_space 
-    var snapped_snap_space_pos: Vector3 = floor(snap_space_pos * texel_snap) / texel_snap
-    cam.global_position = snap_space * snapped_snap_space_pos
-    cam.global_rotation = cam_proxy.global_rotation
-
-    var snap_space_error := snapped_snap_space_pos - snap_space_pos
-    var screen_texel_error := Vector2(snap_space_error.x, -snap_space_error.y) * texel_snap
-    render_texture.position = screen_texel_error
-
+func _process(_delta):
+	if global_rotation != _last_rot:
+		_last_rot = global_rotation
+		_snap_space = global_transform
+	var texel_snap := float(get_viewport().size.y) / size
+	var snap_space_pos := global_position * _snap_space
+	var snapped_snap_space_pos: Vector3 = floor(snap_space_pos * texel_snap) / texel_snap
+	var snap_error := snapped_snap_space_pos - snap_space_pos
+	h_offset = snap_error.x
+	v_offset = snap_error.y
+	# NOTE(david): use error to shift the viewport texture on TextureRect/Sprite3D/etc. for extra smooth
+	texel_error = Vector2(snap_error.x, -snap_error.y) * texel_snap
